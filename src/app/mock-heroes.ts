8def11f9a930082e136af44aea880083be087269
prdxTO <itsbradchow@gmail.com>
2017-11-14 21:45:24 -0500

    End Section 4 Hero Tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Bloodseeker' },
+  { id: 12, name: 'Yunero' },
+  { id: 13, name: 'Brewmaster' },
+  { id: 14, name: 'Drow Ranger' },
+  { id: 15, name: 'Sand King' },
+  { id: 16, name: 'Leoric' },
+  { id: 17, name: 'Windrunner' },
+  { id: 18, name: 'Lina' },
+  { id: 19, name: 'Axe' },
+  { id: 20, name: 'Spiritbreaker' }
+];
