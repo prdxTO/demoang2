3a12e4d17f5930914aa47ea3efce95f9e68b383e
prdxTO <itsbradchow@gmail.com>
2017-11-14 20:46:58 -0500

    End Section 3 Hero Editor Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.css'],
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'Tour of Heroes';
